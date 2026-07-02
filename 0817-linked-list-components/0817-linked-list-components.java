da01499cfd41ebcb33a19ce9a51c74031d39e322
Rajeswari Damam <117435393+damamrajeswari@users.noreply.github.com>
2026-07-02 17:03:27 +0530

    Time: 7 ms (85.37%), Space: 47 MB (73.66%) - LeetHub

---
+++ 0817-linked-list-components/0817-linked-list-components.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int n:nums){
+            set.add(n);
+        }
+        int count=0;
+        while(head!=null){
+           if(set.contains(head.val)){
+            count++;
+           
+           while(head.next!=null&&set.contains(head.next.val)){
+            head=head.next;
+           }
+           }
+           head=head.next;
+        }
+        return count;
+
+    }
+}
